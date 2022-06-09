462d8550604569d25743d24fe22d3a816ff18cb5
RiazK <RiazCKhan@gmail.com>
2022-06-09 14:00:31 -0400

    remove mongoose connect flag - deprecated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -10,6 +11,14 @@ const port = process.env.PORT || 8001;
 // Middleware Config
 app.use(cors());
 app.use(express.json());
+
+// Mongoose Config
+const uri = process.env.ATLUS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 // Start Server
 app.listen(port, () => {
