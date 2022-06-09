cbc9261c2a13b15f3b272692b0b24504bc30f9f5
RiazK <RiazCKhan@gmail.com>
2022-06-09 19:21:21 -0400

    fix update and delete route by adding save()

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -5,14 +5,14 @@ let Client = require('../models/clients.model');
 router.route('/').get((req, res) => {
   Client.find()
     .then(clients => res.json(clients))
-    .catch(error => res.status(400).json('Error: ' + error))
+    .catch(error => res.status(400).json('Error: ' + error));
 });
 
 // Retrieve Single Client
 router.route('/:id').get((req, res) => {
   Client.findById(req.params.id)
     .then(client => res.json(client))
-    .catch(error => res.status(400).json('Error: ' + error))
+    .catch(error => res.status(400).json('Error: ' + error));
 });
 
 // Create New CLient
@@ -27,17 +27,24 @@ router.route('/add').post((req, res) => {
 
   newClient.save()
     .then(() => res.json('Client Added!'))
-    .catch((error) => res.status(400).json('Error: ' + error))
-})
+    .catch((error) => res.status(400).json('Error: ' + error));
+});
 
 // Update Client Details
-router.route('/update/:id').put((req, res) => {
+router.route('/update/:id').patch((req, res) => {
+
+  console.log(req.body);
+
   Client.findById(req.params.id)
     .then((client) => {
       client.anger_count = req.body.anger_count;
       client.relapse_count = req.body.relapse_count;
       client.individual_count = req.body.individual_count;
       client.complete = req.body.complete;
+
+      client.save()
+        .then(() => res.json('Client Updated!'))
+        .catch(error => res.status(400).json('Error: ' + error));
     })
     .catch((error) => res.status(400).json('Error: ' + error));
 });
@@ -47,8 +54,14 @@ router.route('/delete/:id').delete((req, res) => {
   Client.findById(req.params.id)
     .then((client) => {
       client.active = req.body.complete;
+
+      client.save()
+        .then(() => res.json('Client Inactive!'))
+        .catch(error => res.status(400).json('Error: ' + error));
     })
     .catch((error) => res.status(400).json('Error: ' + error));
 });
 
 module.exports = router;
+
+// "62a261d7a868f9a1e75e9da1"
