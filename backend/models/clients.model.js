805521182479bfcb65b688941dc30c46447e3305
RiazK <RiazCKhan@gmail.com>
2022-06-09 17:04:27 -0400

    format for clarity

--- backend/models/clients.model.js
+++ backend/models/clients.model.js
@@ -3,23 +3,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const clientSchema = new Schema({
-  first_name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  last_name: {
-    type: String,
-    required: true,
-    trim: true
-  },
+  first_name: { type: String, required: true, trim: true },
+  last_name: { type: String, required: true, trim: true },
   anger_count: { type: Number },
   relapse_count: { type: Number },
   private_count: { type: Number },
-  complete: {
-    type: Boolean,
-    default: false
-  },
+  complete: { type: Boolean, default: false },
   court_date: { type: Date },
   therapist_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Therapist' }
 });
