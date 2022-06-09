4edf5c318023a8859afe67a3685e2a9c9e664406
RiazK <RiazCKhan@gmail.com>
2022-06-09 13:42:57 -0400

    add comments

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+// Express Server Config
 const app = express();
 const port = process.env.PORT || 8001;
 
+// Middleware Config
 app.use(cors());
 app.use(express.json());
 
+// Start Server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
