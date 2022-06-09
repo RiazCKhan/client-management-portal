75e2d394eaddd64dcbcbda6da32f90f26296e47d
RiazK <RiazCKhan@gmail.com>
2022-06-09 17:24:54 -0400

    update private to individual count

--- backend/models/clients.model.js
+++ backend/models/clients.model.js
@@ -7,7 +7,7 @@ const clientSchema = new Schema({
   last_name: { type: String, required: true, trim: true },
   anger_count: { type: Number },
   relapse_count: { type: Number },
-  private_count: { type: Number },
+  individual_count: { type: Number },
   complete: { type: Boolean, default: false },
   court_date: { type: Date },
   therapist_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Therapist' }
