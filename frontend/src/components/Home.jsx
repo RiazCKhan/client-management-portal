e6a261257c855e5dadcdca5a1c81cd11689329fd
RiazK <RiazCKhan@gmail.com>
2022-06-29 13:30:17 -0400

    inital commit home page and css sheet

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          HOME
+        </h1>
+      </header>
+    </div>
+  )
+}
