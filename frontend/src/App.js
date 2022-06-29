92a50304f4cd2a3ef11cf1ee7a17f1395d8b6e5f
RiazK <RiazCKhan@gmail.com>
2022-06-29 13:55:57 -0400

    add error component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from "./components/Home";
+import Error from "./components/Error"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
