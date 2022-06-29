df0160afbfcd2429665588465d3fd8aed6c89b0a
RiazK <RiazCKhan@gmail.com>
2022-06-29 15:26:54 -0400

    add footer import

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer'
 
 import './Home.css';
 
@@ -8,15 +9,9 @@ export default function Home() {
     <>
       <Navbar />
       <section>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="text-3xl font-bold underline">
-              ERROR
-            </h1>
-          </header>
-        </div>
         <Outlet />
       </section>
+      <Footer />
     </>
   )
 }
