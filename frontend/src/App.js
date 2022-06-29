74ca203dab4a7a2541f7bac39b40b8d75f204dfa
RiazK <RiazCKhan@gmail.com>
2022-06-29 15:26:27 -0400

    remove three links and associated imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from "./pages/Home";
+import Add from "./pages/Add";
 import Edit from "./pages/Edit";
 import Update from "./pages/Update";
 import Session from "./pages/Session";
-import Relapse from "./pages/Relapse";
-import Anger from "./pages/Anger";
-import Individual from "./pages/Individual";
+// import Relapse from "./pages/Relapse";
+// import Anger from "./pages/Anger";
+// import Individual from "./pages/Individual";
 import Notifications from "./pages/Notifications";
-import Home from "./pages/Home";
 import Error from "./pages/Error"
 
 export default function App() {
@@ -16,14 +16,15 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} >
-          <Route path='edit/:id' element={<Edit />} />
-          <Route path='update/:id' element={<Update />} />
-          <Route path='session/:id' element={<Session />} />
-          <Route path='relapseprevention' element={<Relapse />} />
-          <Route path='angermanagement' element={<Anger />} />
-          <Route path='individual' element={<Individual />} />
+          <Route path='add' element={<Add />} />
+          <Route path='edit' element={<Edit />} />
+          <Route path='update' element={<Update />} />
+          <Route path='session' element={<Session />} />
+          {/* <Route path='relapseprevention' element={<Relapse />} /> */}
+          {/* <Route path='angermanagement' element={<Anger />} /> */}
+          {/* <Route path='individual' element={<Individual />} /> */}
           <Route path='notifications' element={<Notifications />} />
-          <Route path='add' element={<Error />} />
+          <Route path='error' element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
