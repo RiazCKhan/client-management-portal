074de9738552b3c0585099be8a4b5c5af3402f2d
RiazK <RiazCKhan@gmail.com>
2022-06-29 09:58:15 -0400

    add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    // Nav
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
