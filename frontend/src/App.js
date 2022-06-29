86083e4ad379d13e17ffa2a4f065296f5bbf6651
RiazK <RiazCKhan@gmail.com>
2022-06-29 14:35:29 -0400

    Update Router Paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from "./components/Home";
-import Error from "./components/Error"
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import Update from "./pages/Update";
+import Session from "./pages/Session";
+import Relapse from "./pages/Relapse";
+import Anger from "./pages/Anger";
+import Individual from "./pages/Individual";
+import Notifications from "./pages/Notifications";
+import Home from "./pages/Home";
+import Error from "./pages/Error"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route path='*' element={<Error />} />
+        <Route path='/' element={<Home />} >
+          <Route path='edit/:id' element={<Edit />} />
+          <Route path='update/:id' element={<Update />} />
+          <Route path='session/:id' element={<Session />} />
+          <Route path='relapseprevention' element={<Relapse />} />
+          <Route path='angermanagement' element={<Anger />} />
+          <Route path='individual' element={<Individual />} />
+          <Route path='notifications' element={<Notifications />} />
+          <Route path='add' element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
