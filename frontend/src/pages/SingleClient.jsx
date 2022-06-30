fbd929a334984084482aa8cf843cd5e4673c23e9
RiazK <RiazCKhan@gmail.com>
2022-06-30 14:15:10 -0400

    inital commit Single Pages

---
+++ frontend/src/pages/SingleClient.jsx
@@ -0,0 +1,13 @@
+import './00.css';
+
+export default function SingleClient() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          SINGLE CLIENT PAGE :ID
+        </h1>
+      </header>
+    </div>
+  )
+}
