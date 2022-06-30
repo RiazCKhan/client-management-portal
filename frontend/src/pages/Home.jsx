ba2b49e4afb3cf467e3323e95f21646e55745fb3
RiazK <RiazCKhan@gmail.com>
2022-06-30 10:38:20 -0400

    removed unused imports

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,3 @@
-import { Link, Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer'
-
 import './Home.css';
 
 export default function Home() {
