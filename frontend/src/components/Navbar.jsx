5622ff55ba509a1730d8abcd8360f1a9d26868d6
RiazK <RiazCKhan@gmail.com>
2022-06-30 10:10:58 -0400

    remove old Navigation code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,13 +12,6 @@ const navigation = [
   {name: 'Find Session', to:'/session', current: false}
 ]
 
-// const navigation = [
-//   { name: 'Dashboard', href: '#', current: true },
-//   { name: 'Team', href: '#', current: false },
-//   { name: 'Projects', href: '#', current: false },
-//   { name: 'Calendar', href: '#', current: false },
-// ]
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
