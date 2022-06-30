c214de0bd0f4c52df4ec1c4edadf4b30ca8f041d
RiazK <RiazCKhan@gmail.com>
2022-06-30 10:27:17 -0400

    inital commit. Content transfer from Home page

---
+++ frontend/src/components/SharedLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer'
+
+export default function SharedLayout() {
+  return (
+    <>
+      <Navbar />
+      <section>
+        <Outlet />
+      </section>
+      <Footer />
+    </>
+  )
+}
