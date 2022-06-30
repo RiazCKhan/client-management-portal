851d68cc480f3cf618afa8923f8cd9597d3d7faa
RiazK <RiazCKhan@gmail.com>
2022-06-30 10:14:50 -0400

    remove focus style on nav notification icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,10 +6,10 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { Link } from "react-router-dom";
 
 const navigation = [
-  {name: 'Add Client' , to: '/add', current: false},
-  {name: 'Edit Client', to:'/edit', current: false},
-  {name: 'Update Client', to:'/update', current: false},
-  {name: 'Find Session', to:'/session', current: false}
+  { name: 'Add Client', to: '/add', current: false },
+  { name: 'Edit Client', to: '/edit', current: false },
+  { name: 'Update Client', to: '/update', current: false },
+  { name: 'Find Session', to: '/session', current: false }
 ]
 
 function classNames(...classes) {
@@ -66,13 +66,15 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+                <Link to='/notifications'>
+                  <button
+                    type="button"
+                    className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none "
+                  >
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </Link>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative">
