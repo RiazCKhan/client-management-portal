e39ae32a88c3f8bc94859fc54fd3b82698f287e4
RiazK <RiazCKhan@gmail.com>
2022-07-11 15:45:06 -0400

    Determine pages to be components. Update App.js to reflect the shift in pages to components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,6 @@ import Error from "./pages/Error"
 import Home from "./pages/Home";
 import Notifications from "./pages/Notifications";
 
-// import Add from "./pages/Add";
-// import Edit from "./pages/Edit";
-// import Program from "./pages/Program";
-// import SingleClient from './pages/SingleClient';
-// import SingleProgram from './pages/SingleProgram';
-// import SingleSession from './pages/SingleSession';
-
 import SharedLayout from './components/SharedLayout';
 
 export default function App() {
@@ -21,20 +14,8 @@ export default function App() {
       <Routes>
         <Route path='/' element={<SharedLayout />} >
           <Route index element={<Home />} />
-
           <Route path='clients' element={<Clients />} />
-          
-          {/* <Route path='add' element={<Add />} /> */}
-
-          {/* <Route path='edit/' element={<Edit />} /> */}
-          {/* <Route path='edit/:clientId' element={<SingleClient />} /> */}
-
           <Route path='attendance/' element={<Attendance />} />
-          {/* <Route path='attendance/:sessionId' element={<SingleSession />} /> */}
-
-          {/* <Route path='activeclients/' element={<Program />} /> */}
-          {/* <Route path='activeclients/:programId' element={<SingleProgram />} /> */}
-
           <Route path='notifications' element={<Notifications />} />
           <Route path='*' element={<Error />} />
         </Route>
